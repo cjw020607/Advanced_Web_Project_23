bf18ca3c1ed80da8ed19e984da3c380bdc722ef8
bamb14 <althcjstk08@gachon.ac.kr>
2023-05-23 17:28:46 +0900

    update api

--- backend/crawling-service/src/main/java/gcu/backend/crawlingservice/CrawlingServiceApplication.java
+++ backend/crawling-service/src/main/java/gcu/backend/crawlingservice/CrawlingServiceApplication.java
@@ -2,8 +2,10 @@ package gcu.backend.crawlingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CrawlingServiceApplication {
 
 	public static void main(String[] args) {
