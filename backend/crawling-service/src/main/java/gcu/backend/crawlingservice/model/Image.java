ed2a840204702745c6d36152f2c8dd4320135cc4
eilsamo600 <eilsamo600@gachon.ac.kr>
2023-05-26 01:54:34 +0900

    update image crawling

---
+++ backend/crawling-service/src/main/java/gcu/backend/crawlingservice/model/Image.java
@@ -0,0 +1,13 @@
+package gcu.backend.crawlingservice.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Image {
+    private String imgurl;
+
+}
