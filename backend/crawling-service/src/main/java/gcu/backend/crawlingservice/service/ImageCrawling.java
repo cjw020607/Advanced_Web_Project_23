783b0ef27da8254a526c1ffa4ba9276f608dc99f
2sojeong <118418288+2sojeong@users.noreply.github.com>
2023-05-26 20:04:09 +0900

    update imgcrawling

--- backend/crawling-service/src/main/java/gcu/backend/crawlingservice/service/ImageCrawling.java
+++ backend/crawling-service/src/main/java/gcu/backend/crawlingservice/service/ImageCrawling.java
@@ -6,12 +6,13 @@ import gcu.backend.crawlingservice.model.Images;
 import lombok.Data;
 
 import java.util.*;
+
 import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;;
+import org.jsoup.select.Elements;
 
 @Service
 public class ImageCrawling {
@@ -23,14 +24,20 @@ public class ImageCrawling {
         for (int i = 0; i < keyword.size(); i++) {
             // url date값 오늘 날짜 불러와서 넣으면 될듯 -> 최신뉴스 반영, 그날 그날 뉴스 반영
             // page값은 1페이지,2페이지,, 이렇게
-            String url = "https://www.shutterstock.com/ko/search/" + keyword.get(i).replace(" ", "").replace("-", "")
-                    + "?image_type=photo";
+            String url = "https://picjumbo.com/search/"
+                    + keyword.get(i).replace(" ", "").replace("-", "");
             Document doc = Jsoup.connect(url).get();
-            Elements imgelement = doc.getElementsByAttributeValue("class", "mui-1l7n00y-thumbnail");
-            String imgUrl = imgelement.attr("src"); // 사진링크
-            System.out.println(imgUrl);
+            Elements imgelement = doc.getElementsByAttributeValue("class", "api_media_wrap");
+
+            Element element = imgelement.get(0);
+            Elements img = element.getElementsByAttributeValue("loading", "eager");
+
+            String imgUrl = img.attr("src");
+
             Image.add(imgUrl);
+
         }
+
         Images images = new Images();
         images.setImgurl(Image);
 
